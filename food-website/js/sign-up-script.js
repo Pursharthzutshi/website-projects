d3a7259de490468bb8443da1ef370a6c21106292
Pursharth Zutshi <pursharthzutshi@gmail.com>
2021-08-31 15:16:38 +0530

    update food website

---
+++ food-website/js/sign-up-script.js
@@ -0,0 +1,5 @@
+const usernameInput = document.querySelector(".username-input");
+const usernameInput = document.querySelector(".username-input");
+const emailIdInput = document.querySelector(".email-id-input");
+const passwordInput = document.querySelector(".password-input");
+const passwordReCheckInput = document.querySelector(".password-re-check-input");
