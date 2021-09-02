f1872f675cc03044f08e717f27c5d14a9412337c
Pursharth Zutshi <pursharthzutshi@gmail.com>
2021-09-02 17:41:49 +0530

    Updating food website

--- food-website/js/mobile-nav-bar-scirpt.js
+++ food-website/js/mobile-nav-bar-scirpt.js
@@ -1,17 +1,18 @@
 const navBarButton = document.querySelector(".nav-bar-button");
 
-navBarButton.addEventListener("click", showMenuBar);
+navBarButton.addEventListener("click", showMenuBar, true);
 
 
 function showMenuBar() {
 
     removenavBarContainerIconDiv = document.createElement("div");
     removenavBarContainerIconDiv.classList.add("remove-nav-bar-container-icon-div");
+    removenavBarContainerIconDiv.setAttribute("style", "display:flex;flex-direction:row;justify-content:flex-end;padding-top:30px;");
 
     const removenavBarContainerIcon = document.createElement("button");
     removenavBarContainerIcon.classList.add("remove-nav-bar-container-icon");
     removenavBarContainerIcon.innerHTML = '<i class="fa fa-times"></i>';
-
+    removenavBarContainerIcon.setAttribute("style", "margin:-20px 0px 0px -30px;color:red;");
 
     const menuBarHeading = document.createElement("h3");
     menuBarHeading.classList.add("menu-bar-heading");
@@ -21,11 +22,11 @@ function showMenuBar() {
 
     const navBar = document.querySelector(".nav-bar");
 
-    navBar.style.visibility = 'visible';
+    navBarContainer.style.visibility = 'hidden';
 
     navBarContainer.style.visibility = 'visible';
 
-    menuBarHeading.style.pointerEvents = 'none';
+    document.body.style.background = 'linear-gradient(to left,rgba(232, 232, 232, 1),rgba(0,0,0,0.1))';
 
     removenavBarContainerIconDiv.appendChild(removenavBarContainerIcon);
 
@@ -34,5 +35,15 @@ function showMenuBar() {
     navBarContainer.appendChild(menuBarHeading);
 
     navBarContainer.appendChild(navBar);
+
+    removenavBarContainerIcon.addEventListener("click", removeNavBar);
+
+    navBar.style.visibility = 'visible';
+
+    function removeNavBar() {
+        navBarContainer.removeChild(navBar);
+        menuBarHeading.style.color = 'transparent';
+        navBarContainer.style.visibility = 'hidden';
+    }
 
 }
