1c31512079c6ad571d9cd58ab368c62ec75b39cc
Pursharth Zutshi <pursharthzutshi@gmail.com>
2021-09-11 03:30:15 +0530

    updating food website

--- food-website/js/feedback-box-script.js
+++ food-website/js/feedback-box-script.js
@@ -17,19 +17,39 @@ function createBox(){
     removeFeedbackBoxIcon.classList.add("remove-feedback-box-icon");
     removeFeedbackBoxIcon.innerHTML='<ion-icon name="close"></ion-icon>';
 
+
     feedbackBox.appendChild(removeFeedbackBoxIcon);
 
     feedbackBoxDiv.appendChild(feedbackBox);
 
     removeFeedbackBoxIcon.addEventListener("click",removeBox);
 
-    feedbackBox.pointerEvents.style='auto';
 
     function removeBox(){
 
         feedbackBoxDiv.setAttribute("style","width: 27rem;height:22rem;background:white;visibility:hidden")
 
         removeFeedbackBoxIcon.pointerEvents.style='none';
+
+    }
+
+    const addFeedbackBoxButton = document.querySelector('.add-feedback-box-button');
+
+    addFeedbackBoxButton.addEventListener('click',giveFeedbackBox);
+
+    function giveFeedbackBox(){
+
+        const giveYourFeedbackBox = document.createElement("div");
+        giveYourFeedbackBox.classList.add("give-feedback-box");
+
+        const nameInput = document.createElement('input');
+        nameInput.classList.add('feedback-name-input');
+
+        document.body.appendChild(giveYourFeedbackBox);
+
+        giveYourFeedbackBox.appendChild(nameInput);
+
+        document.body.style.background = 'linear-gradient(to left,rgba(232, 232, 232, 1),rgba(0,0,0,0.1))';
 
     }
 }
