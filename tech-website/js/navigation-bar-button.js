dc620acf84aa4d8bc04397d089cf1fa06bff70ff
Pursharth Zutshi <pursharthzutshi@gmail.com>
2021-09-20 13:32:54 +0530

    Changes

--- tech-website/js/navigation-bar-button.js
+++ tech-website/js/navigation-bar-button.js
@@ -2,13 +2,13 @@ main();
 
 function main() {
 
+
     const navBarButton = document.createElement("button");
     navBarButton.classList.add("nav-bar-button");
     navBarButton.innerHTML = "<ion-icon name='menu-outline'></ion-icon>";
 
     navBarButton.addEventListener("click", showNavBar);
 
-    navBarButton.style.visibility = "hidden";
 
     document.body.appendChild(navBarButton);
 
@@ -18,21 +18,16 @@ function main() {
         button[i].addEventListener("click", link);
     }
 
-    window.addEventListener("scroll", function () {
-
-        const plansSection = document.querySelector(".plans-section");
-
-        if (plansSection.getBoundingClientRect().top < window.innerHeight) {
-            navBarButton.style.visibility = "visible";
-        }
-    });
-
 }
 
 
 
 
 function showNavBar() {
+
+    const navBarList = document.querySelector(".nav-bar-list");
+
+    navBarList.style.visibility='visible';
 
     const showNavBar = document.createElement("div");
     showNavBar.classList.add("show-nav-bar");
@@ -48,13 +43,22 @@ function showNavBar() {
 }
 
 function link() {
-    const header = document.querySelector(".header");
+    const navBarContainer = document.querySelector(".nav-bar-container");
     const navBar = document.querySelector(".nav-bar");
     const showNavBar = document.querySelector(".show-nav-bar");
+    const navBarList = document.querySelector(".nav-bar-list");
 
+    const mq = window.matchMedia( "(max-width: 704px)" );
+
+    if(mq.matches){
+        navBarList.style.visibility='hidden';
+    }else{
+        navBarList.style.visibility='visible';
+    }
     document.body.removeChild(showNavBar);
 
-    header.appendChild(navBar);
+    navBarContainer.appendChild(navBar);
+
 
 
 }
