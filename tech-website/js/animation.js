34258cc47727ed772b256264651ab9201b35c32b
Pursharth Zutshi <pursharthzutshi@gmail.com>
2021-09-25 01:40:56 +0530

    adding animation feature

--- tech-website/js/animation.js
+++ tech-website/js/animation.js
@@ -1,25 +1,25 @@
 const featuresSection = document.querySelector(".features-section");
-const headerImg =document.querySelector(".header-img");
+const headerImg = document.querySelector(".header-img");
 
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     const value = window.scrollY;
     headerImg.style.marginLeft = value * 1.5 + "px";
 
 });
 
-window.addEventListener("scroll",function(){
-    if(featuresSection.getBoundingClientRect().top < window.innerHeight){
+window.addEventListener("scroll", function () {
+    if (featuresSection.getBoundingClientRect().top < window.innerHeight) {
         featuresSection.style.transform = "scale(1)";
-    }else{
+    } else {
         featuresSection.style.transform = "scale(.9)";
     }
 })
 
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     AOS.init({
-        offset:300,
-        delay:100,
-        duration:100,
+        offset: 300,
+        delay: 100,
+        duration: 100,
     });
 })
 
